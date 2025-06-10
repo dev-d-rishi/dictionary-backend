ec3f8b41d330d6d51bcd4f942f0d2597812060f3
rishu-ish <167009377+rishu-ish@users.noreply.github.com>
2025-06-11 03:29:14 +0530

    Remove cors

--- src/app.ts
+++ src/app.ts
@@ -9,23 +9,23 @@ const limiter = rateLimit({
 
 const app = express();
 
-const allowedOrigins = [
-    "https://grab-vocab.vercel.app",   // replace with your frontend domain
-    "http://localhost:5173",          // replace with your frontend domain
-  ];
+// const allowedOrigins = [
+//     "https://grab-vocab.vercel.app",   // replace with your frontend domain
+//     "http://localhost:5173",          // replace with your frontend domain
+//   ];
   
-  const corsOptions: cors.CorsOptions = {
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or curl)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    }
-  };
+  // const corsOptions: cors.CorsOptions = {
+  //   origin: (origin, callback) => {
+  //     // Allow requests with no origin (like mobile apps or curl)
+  //     if (!origin || allowedOrigins.includes(origin)) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error("Not allowed by CORS"));
+  //     }
+  //   }
+  // };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.set('trust proxy', 1);
 // app.use(limiter); // Apply rate limiting to all requests
