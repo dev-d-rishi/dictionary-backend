35377669cec9b46cfe098ccb2be3288ba22803c7
rishu-ish <167009377+rishu-ish@users.noreply.github.com>
2025-06-14 01:05:08 +0530

    commands

--- src/server.ts
+++ src/server.ts
@@ -36,8 +36,9 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  // src/server.ts
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running on port ${process.env.PORT || 5000}`);
   });
 });
 
